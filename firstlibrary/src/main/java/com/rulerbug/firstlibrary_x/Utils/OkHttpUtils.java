448fdb4d634f91fb5caf0c21af4fa07e7d2ac257
JiJiBo <1270071138@qq.com>
2020-01-19 11:21:40 +0800

    优化okhttp

--- firstlibrary/src/main/java/com/rulerbug/firstlibrary_x/Utils/OkHttpUtils.java
+++ firstlibrary/src/main/java/com/rulerbug/firstlibrary_x/Utils/OkHttpUtils.java
@@ -3,6 +3,7 @@ package com.rulerbug.firstlibrary_x.Utils;
 import java.io.InputStream;
 import java.util.HashMap;
 
+import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -72,6 +73,30 @@ public class OkHttpUtils {
         return null;
     }
 
+    public static String postHttp(String url, RequestBody body) {
+//        val jsonStr = OkHttpUtils.postString(
+//                HttpUrlArgument.POST_EAR_TAG_INFO,
+//                FormBody.Builder().add("", "").build()
+//        )
+        try {
+            OkHttpClient client = new OkHttpClient();
+            //2. 创建请求的Request 对象
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+
+            //3. 在Okhttp中创建Call 对象，将request和Client进行绑定
+            //4. 执行Call对象（call 是interface 实际执行的是RealCall）中的`execute`方法
+
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public static String postString(String url, HashMap<String, String> paramsMap) {
         try {
             FormBody.Builder builder = new FormBody.Builder();
@@ -96,5 +121,27 @@ public class OkHttpUtils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static void postHttp(String url, RequestBody body, Callback callbcak) {
+//        val jsonStr = OkHttpUtils.postString(
+//                HttpUrlArgument.POST_EAR_TAG_INFO,
+//                FormBody.Builder().add("", "").build()
+//        )
+        try {
+            OkHttpClient client = new OkHttpClient();
+            //2. 创建请求的Request 对象
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+
+            //3. 在Okhttp中创建Call 对象，将request和Client进行绑定
+            //4. 执行Call对象（call 是interface 实际执行的是RealCall）中的`execute`方法
+
+            client.newCall(request).enqueue(callbcak);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
